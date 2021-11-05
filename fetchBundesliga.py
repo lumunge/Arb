9f535350eb90e010bd1ca4de7e314a415a66e7a0
Erick Lumunge <lumungep12@gmail.com>
2021-11-05 22:55:41 +0300

    color and filtering

---
+++ fetchBundesliga.py
@@ -0,0 +1,9 @@
+import requests
+import json
+
+# Bundesliga URLS
+BetikaPLURL = "https://www.betika.com/s/soccer/germany-bundesliga"
+SportPesaPLURL = "https://www.ke.sportpesa.com/sports?sportId=1&section=top&leagueId=76390&name=Bundesliga&paginationOffset=0"
+Bet22URL = "https://22bet.co.ke/line/Football/96463-Germany-Bundesliga/"
+MelBetUrl = "https://melbet.ke/line/football/96463-germany-bundesliga/"
+X1BetURL = "https://1xbet.co.ke/line/Football/96463-Germany-Bundesliga/"
