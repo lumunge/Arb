dd0d600d5e0d0a8c31decf93d52e907c8a82d960
lumunge <lumungep12@gmail.com>
2022-04-06 02:38:49 +0300

    database

---
+++ database/clearBlTables.py
@@ -0,0 +1,23 @@
+import conn as dbConn
+
+def truncateBlTables():
+    db = "./bundesliga.db"
+    conn = dbConn.createConnection(db)
+    spSql = """DELETE FROM sportpesaBundesliga"""
+    btkSql = """DELETE FROM betikaBundesliga"""
+    bt22Sql = """DELETE FROM bet22Bundesliga"""
+    mlSql = """DELETE FROM melBundesliga"""
+    x1Sql = """DELETE FROM x1betBundesliga"""
+    combsSql = """DELETE FROM bLCombinations"""
+    cur = conn.cursor()
+    cur.execute(spSql)
+    cur.execute(btkSql)
+    cur.execute(bt22Sql)
+    cur.execute(mlSql)
+    cur.execute(combsSql)
+    cur.execute(x1Sql)
+    conn.commit()
+    return cur.lastrowid
+
+
+truncateBlTables()
