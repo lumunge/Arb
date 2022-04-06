5d2e9d41684c3539f75037d04a64d180c882ef14
lumunge <lumungep12@gmail.com>
2022-04-06 23:38:55 +0300

    odds

---
+++ calcOdds/calcPL.py
@@ -0,0 +1,74 @@
+import os
+import sys
+
+DBPATH = os.path.abspath(os.path.join(
+    os.path.dirname(__file__), 
+    os.pardir))
+sys.path.append(DBPATH)
+
+import utils
+import database
+
+def getRecords():
+    db = "../database/premierLeague.db"
+    conn = database.createConnection(db)
+    getSpSql = """SELECT * FROM pLCombinations"""
+    cur = conn.cursor()
+    cur.execute(getSpSql)
+    results = cur.fetchall()
+    combinations = utils.genIndices()
+    conn.commit()
+    sites = {
+        2: "sph",
+        3: "spx",
+        4: "spa",
+        5: "btkh",
+        6: "btkx",
+        7: "btka",
+        8: "bt22h",
+        9: "bt22x",
+        10: "bt22a",
+        11: "mlh",
+        12: "mlx",
+        13: "mla",
+        14: "x1h",
+        15: "x1x",
+        16: "x1a",
+    }
+    print(
+        "{:<10} {:<10} {:<5} {:<10} {:<5} {:<10} {:<5} {:<10} {:<10} {:<15}".format(
+            "Home_Team",
+            "Away_Team",
+            "site",
+            "hOdd",
+            "site",
+            "nOdd",
+            "site",
+            "aOdd",
+            "ArbProb",
+            "Time",
+        )
+    )
+    for i in results:
+        for j in combinations:
+            print(
+                "{:<10} {:<10} {:<5} {:<10} {:<5} {:<10} {:<5} {:<10} {:<10} {:<15}".format(
+                    i[0],
+                    i[1],
+                    sites[j[0]],
+                    i[j[0]],
+                    sites[j[1]],
+                    i[j[1]],
+                    sites[j[2]],
+                    i[j[2]],
+                    utils.probability(i[j[0]], i[j[1]], i[j[2]]),
+                    i[17],
+                )
+            )
+        print(
+            "------------------------------------------------------------------------------------------------------------------"
+        )
+    return cur.lastrowid
+
+
+getRecords()
